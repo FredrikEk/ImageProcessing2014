b62b3827119868c2ae1789a3eff0ff8352fd6e1f
Fredrik Ek <fredrik.ek.s@gmail.com>
2014-09-18 10:30:18 +0200

    Homework corrected

--- Lecture Notes/HomeworkLv3.m
+++ Lecture Notes/HomeworkLv3.m
@@ -1,4 +1,7 @@
 %% Homework
+clear
+clf
+
 time = 1:256;
 signal = cos(2*pi*(2/256)*time)+randn(1,256);
 plot(signal)
@@ -9,7 +12,9 @@ dwtmode('per')
 figure, plot(C)
 
 %%
-figure, subplot(3,3,1), plot (signal)
+figure, subplot(3,3,1), plot(signal)
 for level=1:8
     C(256/2^level+1:256)=0;
-    smoothed_signal
+    smoothed_signal = waverec(C,L,'rbio6.8');
+    subplot(3,3,(level+1)), plot(smoothed_signal)
+end
